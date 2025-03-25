750406f97ac4c7752b4fc8c5936d88fbf45b4b72
Zcr4zy <carlinhoscunhaig@gmail.com>
2025-03-25 00:43:00 -0300

    Finalização documento PDF Aula

--- 03-130125/js/main.js
+++ 03-130125/js/main.js
@@ -1,3 +1,14 @@
+
+const options = {
+    method: 'GET',
+    headers:{
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YmFjMTQ5NGM3NGM4NTZhNTJjZmFjMTllN2M2NjRjYSIsIm5iZiI6MTc0Mjg3MDc2Ny44MTIsInN1YiI6IjY3ZTIxOGVmNGM1Mjc0NjY2NWRjNzc0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hKkscFv4J3_bT6mZBvu-ZtRD4xfVN22kuA_2lbyEmtg'
+    }
+};
+
+
+
 function toggleLoading(){
     let loader = document.querySelector(".loader");
     loader.style.display = loader.style.display == 'none' ? 'block' : 'none';
